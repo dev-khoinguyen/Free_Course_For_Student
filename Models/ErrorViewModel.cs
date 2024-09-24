3e9fa9519a20b2f4b8eed525d1470e98871e8cfc
Nguyễn Văn Khởi <khoinv.it@gmail.com>
2024-09-24 21:52:14 +0700

    push all

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EXE_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
