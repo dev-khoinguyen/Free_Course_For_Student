87d2a33a4c72cc510e4ae95ee9d51d414cf70615
DeWick <Ducsieuda@gmail.com>
2025-02-09 14:54:57 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,13 @@ namespace EXE_PROJECT
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // Thời gian hết hạn session
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,8 @@ namespace EXE_PROJECT
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
