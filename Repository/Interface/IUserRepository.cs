37646b8adb6ee058b0e317404ea7369a56534d03
DeWick <Ducsieuda@gmail.com>
2025-02-09 12:29:56 +0700

    add-repository

---
+++ Repository/Interface/IUserRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Free_Course_For_Student.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        
+    }
+}
